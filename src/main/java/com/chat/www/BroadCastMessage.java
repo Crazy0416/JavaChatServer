18437a71527ada06670b8217c5a3f62fb517b5eb
kimminho <minmin0416@naver.com>
2019-01-14 17:50:15 +0900

    BroadCastMessage: 채팅방 유저의 메세지 전달이 되지 않는 오류 발생. socket OutputStream을 PrintWriter로 변경. 해결.

--- src/main/java/com/chat/www/BroadCastMessage.java
+++ src/main/java/com/chat/www/BroadCastMessage.java
@@ -2,6 +2,8 @@ package com.chat.www;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Hashtable;
 import java.util.Iterator;
@@ -26,16 +28,18 @@ public class BroadCastMessage implements Runnable {
 
         while (itr.hasNext()) {
             str = itr.next();
-            OutputStream userOutStream;
+            PrintWriter pw;
 
             try { // TODO: 한명에게 오류가 발생했을 때 어떻게 오류처리 할 것인지 생각하기.
                 Socket userSocket = this.userList.get(str);
-                userOutStream = userSocket.getOutputStream();
+                pw = new PrintWriter(new OutputStreamWriter(userSocket.getOutputStream()));
 
-                userOutStream.write(message.getBytes());
+                pw.println(message);
+                pw.flush();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        System.out.println("메세지 전송 완료");
     }
 }
